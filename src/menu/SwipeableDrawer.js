92c4fd5a450f2914987d9cdc7874498fd9267f1b
DamianMilecki <damian.milecki@gmail.com>
2020-01-21 10:22:33 +0100

    FZ12NP-Firebase refator menu code for sigin

--- src/menu/SwipeableDrawer.js
+++ src/menu/SwipeableDrawer.js
@@ -19,124 +19,176 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles({
-  list: {
-    width: 350,
-  },
-  fullList: {
-    width: 'auto',
-  },
+	list: {
+		width: 350,
+	},
+	fullList: {
+		width: 'auto',
+	},
 });
 
 export default function SwipeableTemporaryDrawer(props) {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    left: false,
-  });
+	const classes = useStyles();
+	const [state, setState] = React.useState({
+		left: false,
+	});
 
- const toggleDrawer = (side, open) => event => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-    setState({ ...state, [side]: open });
-  };
+	const toggleDrawer = (side, open) => event => {
+		if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+		setState({ ...state, [side]: open });
+	};
 
-  let sideList;
-  if (!props.auth) {
-    sideList = (side) => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      
-      <List>
-          <>
-            <ListItem button key={"Lista ciast"} component={Link} to='/cakes'>
-              <ListItemIcon><CakeIcon/></ListItemIcon>
-              <ListItemText primary={'Lista ciast'} />
-            </ListItem>
-          </>
-          <>
-            <ListItem button key={"Lista cukierników"} component={Link} to='/cooks'>
-              <ListItemIcon><EmojiPeopleIcon/></ListItemIcon>
-              <ListItemText primary={'Lista cukierników'} />
-            </ListItem>
-          </>
-      </List>
-      <Divider />
-      <List>
-          <>
-            <ListItem button key={'Zaloguj'} onClick={props.log} component={Link} to='/SignIn'>
-              <ListItemIcon><LockOpenIcon/></ListItemIcon>
-              <ListItemText primary={'Zaloguj'}  />
-            </ListItem>
-          </>
-      </List>
 
-    </div>
-  )} else {
-    sideList = (side) => (
-      <div
-        className={classes.list}
-        role="presentation"
-        onClick={toggleDrawer(side, false)}
-        onKeyDown={toggleDrawer(side, false)}
-      >
-        
-      <List>
-          <>
-          <ListItem button key={"Lista ciast"} component={Link} to='/cakes'>
-            <ListItemIcon><CakeIcon/></ListItemIcon>  
-            <ListItemText primary={'Lista ciast'} />
-          </ListItem>
-          </>
-          <>
-          <ListItem button key={"Lista cukierników"} component={Link} to='/cooks'>
-            <ListItemIcon><EmojiPeopleIcon/></ListItemIcon>
-            <ListItemText primary={'Lista cukierników'} />
-          </ListItem>
-          </>
-      </List>
-      <Divider />
-      <List>
-          <>
-            <ListItem button key={'Moje konto'} component={Link} to='/userCard'>
-              <ListItemIcon><FaceIcon/></ListItemIcon>
-              <ListItemText primary={'Moje konto'} />
-            </ListItem>
-          </>
-          <>
-            <ListItem button key={'Dodaj ciasto'} component={Link} to='/cakeAdd/empty'>
-              <ListItemIcon><ControlPointIcon/></ListItemIcon>
-              <ListItemText primary={'Dodaj ciasto'} />
-            </ListItem>
-          </>
-          <>
-            <ListItem button key={'Wyloguj'} onClick={props.log} component={Link} to='/'>
-              <ListItemIcon><ExitToAppIcon/></ListItemIcon>
-              <ListItemText primary={'Wyloguj'} />
-            </ListItem>
-          </>
-        </List>
-      </div>
-    )
-  };
+	const sideList = () => {
+		return (
+			<div
+				className={classes.list}
+			>
 
-  return (
-    <div>
-       <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer('left', true)}>
-            <MenuIcon />
-          </IconButton>
-      
-      <SwipeableDrawer
-        open={state.left}
-        onClose={toggleDrawer('left', false)}
-        onOpen={toggleDrawer('left', true)}
-      >
-        {sideList('left')}
-      </SwipeableDrawer>
-      
-    </div>
-  );
+				<List>
+					<ListItem button key={"Lista ciast"} component={Link} to='/cakes'>
+						<ListItemIcon><CakeIcon /></ListItemIcon>
+						<ListItemText primary={'Lista ciast'} />
+					</ListItem>
+					<ListItem button key={"Lista cukierników"} component={Link} to='/cooks'>
+						<ListItemIcon><EmojiPeopleIcon /></ListItemIcon>
+						<ListItemText primary={'Lista cukierników'} />
+					</ListItem>
+				</List>
+				<Divider />
+				<List>
+					{!props.auth
+						? <div>
+							<ListItem button key={'Moje konto'} component={Link} to='/userCard'>
+								<ListItemIcon><FaceIcon /></ListItemIcon>
+								<ListItemText primary={'Moje konto'} />
+							</ListItem>
+							<ListItem button key={'Dodaj ciasto'} component={Link} to='/cakeAdd/empty'>
+								<ListItemIcon><ControlPointIcon /></ListItemIcon>
+								<ListItemText primary={'Dodaj ciasto'} />
+							</ListItem>
+							<ListItem button key={'Wyloguj'} onClick={props.log} component={Link} to='/'>
+								<ListItemIcon><ExitToAppIcon /></ListItemIcon>
+								<ListItemText primary={'Wyloguj'} />
+							</ListItem>
+						</div>
+						: <div>
+							<ListItem button key={'Zaloguj'} onClick={props.log} component={Link} to='/SignIn'>
+								<ListItemIcon><LockOpenIcon /></ListItemIcon>
+								<ListItemText primary={'Zaloguj'} />
+							</ListItem>
+						</div>
+					}
+
+
+				</List>
+			</div>
+		)
+	}
+
+	// let sideList;
+	// if (!props.auth) {
+	// 	sideList = (side) => (
+	// 		<div
+	// 			className={classes.list}
+	// 		// role="presentation"
+	// 		// onClick={toggleDrawer(side, false)}
+	// 		// onKeyDown={toggleDrawer(side, false)}
+	// 		>
+
+	// 			<List>
+	// 				<>
+	// 					<ListItem button key={"Lista ciast"} component={Link} to='/cakes'>
+	// 						<ListItemIcon><CakeIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Lista ciast'} />
+	// 					</ListItem>
+	// 				</>
+	// 				<>
+	// 					<ListItem button key={"Lista cukierników"} component={Link} to='/cooks'>
+	// 						<ListItemIcon><EmojiPeopleIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Lista cukierników'} />
+	// 					</ListItem>
+	// 				</>
+	// 			</List>
+	// 			<Divider />
+	// 			<List>
+	// 				<>
+	// 					<ListItem button key={'Zaloguj'} onClick={props.log} component={Link} to='/SignIn'>
+	// 						<ListItemIcon><LockOpenIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Zaloguj'} />
+	// 					</ListItem>
+	// 				</>
+	// 			</List>
+
+	// 		</div>
+	// 	)
+	// } else {
+	// 	sideList = (side) => (
+	// 		<div
+	// 			className={classes.list}
+	// 		// role="presentation"
+	// 		// onClick={toggleDrawer(side, false)}
+	// 		// onKeyDown={toggleDrawer(side, false)}
+	// 		>
+
+	// 			<List>
+	// 				<>
+	// 					<ListItem button key={"Lista ciast"} component={Link} to='/cakes'>
+	// 						<ListItemIcon><CakeIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Lista ciast'} />
+	// 					</ListItem>
+	// 				</>
+	// 				<>
+	// 					<ListItem button key={"Lista cukierników"} component={Link} to='/cooks'>
+	// 						<ListItemIcon><EmojiPeopleIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Lista cukierników'} />
+	// 					</ListItem>
+	// 				</>
+	// 			</List>
+	// 			<Divider />
+	// 			<List>
+	// 				<>
+	// 					<ListItem button key={'Moje konto'} component={Link} to='/userCard'>
+	// 						<ListItemIcon><FaceIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Moje konto'} />
+	// 					</ListItem>
+	// 				</>
+	// 				<>
+	// 					<ListItem button key={'Dodaj ciasto'} component={Link} to='/cakeAdd/empty'>
+	// 						<ListItemIcon><ControlPointIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Dodaj ciasto'} />
+	// 					</ListItem>
+	// 				</>
+	// 				<>
+	// 					<ListItem button key={'Wyloguj'} onClick={props.log} component={Link} to='/'>
+	// 						<ListItemIcon><ExitToAppIcon /></ListItemIcon>
+	// 						<ListItemText primary={'Wyloguj'} />
+	// 					</ListItem>
+	// 				</>
+	// 			</List>
+	// 		</div>
+	// 	)
+	// };
+
+	return (
+		<div>
+			<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer('left', true)}>
+				<MenuIcon />
+			</IconButton>
+
+			<SwipeableDrawer
+				open={state.left}
+				onClose={toggleDrawer('left', false)}
+				onOpen={toggleDrawer('left', true)}
+				onClick={toggleDrawer('left', false)}
+				onKeyDown={toggleDrawer('left', false)}
+				role="presentation"
+			>
+				{sideList()}
+			</SwipeableDrawer>
+
+		</div>
+	);
 }
--- src/menu/resMenu/MenuAppBar.js
+++ src/menu/resMenu/MenuAppBar.js
@@ -4,136 +4,134 @@ import { AppBar } from '@material-ui/core/';
 import BigLogOut from './BigLogOut';
 import BigLogIn from './BigLogIn';
 import SmallLogIn from './SmallLogIn';
-import SmallLogOut from'./SmallLogOut';
+import SmallLogOut from './SmallLogOut';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { withRouter } from 'react-router-dom';
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    
-  },
-  logo: {
-    maxWidth: '170px',
-    paddingLeft: '45px',
-  },
-  label: {
-    fontSize: 15, 
-  },
-  
-  navStyle: {
-    top: 0,
-    left: "auto",
-    right: 0,
-    position: "fixed",
-    color: "#757575",
-    zIndex: 100,
-    minWidth: '300px',    
-  },
-  flex: {
-    display: "flex",
-    justifyContent: "space-between"
-  }
+	root: {
+		flexGrow: 1,
+	},
+	logo: {
+		maxWidth: '170px',
+		paddingLeft: '45px',
+	},
+	label: {
+		fontSize: 15,
+	},
+
+	navStyle: {
+		top: 0,
+		left: "auto",
+		right: 0,
+		position: "fixed",
+		color: "#757575",
+		zIndex: 100,
+		minWidth: '300px',
+	},
+	flex: {
+		display: "flex",
+		justifyContent: "space-between"
+	}
 }));
 
 function MenuAppBar(props) {
-  const classes = useStyles();
-  const matches = useMediaQuery('(max-width:600px)');
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [myStyle, setMystyle] = React.useState('rgba(255,255,255, 0.3)'); //to jest do window.scroll
-  const open = Boolean(anchorEl);
-  const [auth, setAuth] = React.useState(false);
-  // let style = props.styleColor;
-// let style = {backgroundColor:'rgba(255,255,255, 0.3)'};
+	const classes = useStyles();
+	const matches = useMediaQuery('(max-width:600px)');
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [myStyle, setMystyle] = React.useState('rgba(255,255,255, 0.3)'); //to jest do window.scroll
+	const open = Boolean(anchorEl);
+	const [auth, setAuth] = React.useState(false);
+	// let style = props.styleColor;
+	// let style = {backgroundColor:'rgba(255,255,255, 0.3)'};
 
-// to chyba niepotrzbene ale nie wiem   
-// const changeCol = () => {
-//    if (window.pageYOffset <10) {
-//      style={backgroundColor:'white'};
-//    } else {
-//     style={backgroundColor:'red'}; 
-//    }
-// }
+	// to chyba niepotrzbene ale nie wiem   
+	// const changeCol = () => {
+	//    if (window.pageYOffset <10) {
+	//      style={backgroundColor:'white'};
+	//    } else {
+	//     style={backgroundColor:'red'}; 
+	//    }
+	// }
 
-//ta funckja jest do window.scroll
-useEffect(() => {
-  window.addEventListener("scroll", handleScroll);
-  return () => {
-    window.removeEventListener("scroll", handleScroll);
-  };
-});
+	//ta funckja jest do window.scroll
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	});
 
-//ta funckja jest do window.scroll
-const handleScroll = () => {
-  setMystyle(
-    window.pageYOffset===0 ? 'rgba(255,255,255, 0.3)' : 'white',
-  ); 
-};
+	//ta funckja jest do window.scroll
+	const handleScroll = () => {
+		setMystyle(
+			window.pageYOffset === 0 ? 'rgba(255,255,255, 0.3)' : 'white',
+		);
+	};
 
-  const handleChange = () => {
-    setAuth(!auth);
-    setAnchorEl(null);
-    props.history.push('/')
-  };
+	const handleChange = () => {
+		setAuth(!auth);
+		setAnchorEl(null);
+		props.history.push('/')
+	};
 
-  const handleMenu = event => {
-    setAnchorEl(event.currentTarget)
-  };
+	const handleMenu = event => {
+		setAnchorEl(event.currentTarget)
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  };
+	const handleClose = () => {
+		setAnchorEl(null)
+	};
 
-  
-  const log = () => {
-    setAuth(!auth);
-    //props.history.push('/')
-  }
-  
-  let content;
-  
-      if (auth && matches) {
-          content = <SmallLogIn 
-                      auth={auth} 
-                      log={log} 
-                      handleChange={handleChange} 
-                      handleClose={handleClose} 
-                      handleMenu={handleMenu} 
-                      open={open} 
-                      anchorEl={anchorEl}
-                    />;
-      };
-      if (!auth && matches) {
-          content = <SmallLogOut 
-                      auth={auth} 
-                      log={log} 
-                      handleChange={handleChange} 
-                      handleClose={handleClose} 
-                      handleMenu={handleMenu}
-                    />
-      };
-      if (auth&&!matches) {
-          content = <BigLogIn log={log}/>
-      };
-      if (!auth&&!matches) {
-          content = <BigLogOut log={log}/>
-         
-      }
-      
 
-  return (
-    
-    <div className={classes.root} position="static">
-      
-      <AppBar className={classes.navStyle} style={{backgroundColor: myStyle}} >  
-       
-           {content}
-      
-      </AppBar>
-     
-    </div>
-  );
+	const log = () => {
+		setAuth(!auth);
+		//props.history.push('/')
+	}
+
+	let content;
+
+	if (auth && matches) {
+		content = <SmallLogIn
+			auth={auth}
+			log={log}
+			handleChange={handleChange}
+			handleClose={handleClose}
+			handleMenu={handleMenu}
+			open={open}
+			anchorEl={anchorEl}
+		/>;
+	};
+	if (!auth && matches) {
+		content = <SmallLogOut
+			auth={auth}
+			log={log}
+			handleChange={handleChange}
+			handleClose={handleClose}
+			handleMenu={handleMenu}
+		/>
+	};
+	if (auth && !matches) {
+		content = <BigLogIn log={log} />
+	};
+	if (!auth && !matches) {
+		content = <BigLogOut log={log} />
+	}
+
+
+	return (
+
+		<div className={classes.root} position="static">
+
+			<AppBar className={classes.navStyle} style={{ backgroundColor: myStyle }} >
+
+				{content}
+
+			</AppBar>
+
+		</div>
+	);
 }
 
 export default withRouter(MenuAppBar);
