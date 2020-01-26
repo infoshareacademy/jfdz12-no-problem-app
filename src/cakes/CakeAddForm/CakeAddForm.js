2a7bf1ac0dff92b713e3e7a45a7af934eaae948f
Damian Milecki <damian.milecki@gmail.com>
2020-01-26 11:25:53 +0100

    refactor for AddCakeForm

--- src/cakes/CakeAddForm/CakeAddForm.js
+++ src/cakes/CakeAddForm/CakeAddForm.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {styles} from '../CakeStyles';
-import { Button, CardMedia, withStyles, Paper, Grid, Container, CircularProgress, } from '@material-ui/core';
-import CakeAddInput from './CakeAddInput';
-import CakeAddTypesSelect from './CakeAddTypesSelect';
-import CakeAddSelect from './CakeAddSelect';
-import {YESNOSELECT} from '../../constans/selectConstans'
-import CookLabelFull from '../CookLabelFull';
+import { CircularProgress, } from '@material-ui/core';
 import { CAKEADDOBJ } from '../../constans/emptyObject'
 import { getFullData } from '../../api/Api2';
 import PageWrapper from '../../components/PageWrapper';
 import firebase from 'firebase';
 import { Redirect, Link } from 'react-router-dom';
+import RenderCakeAddForm from './RenderCakeAddform'; 
+
 
 class CakeAddForm extends React.Component{
     constructor(props){
@@ -67,9 +63,7 @@ class CakeAddForm extends React.Component{
                         isError: true,
                         error: ' nie ma takiego ciasta '
                     })
-                }
-                
-                
+                }  
             })
             .catch(error => {
                 console.log('bład addformfetch', error.toString())
@@ -102,7 +96,7 @@ class CakeAddForm extends React.Component{
             })
     }
 
-    handleCakeChange =(event) => {
+    handleCakeChange = (event) => {
         const {name, value} = event.target; 
         
         this.setState(prevState => ({
@@ -117,7 +111,6 @@ class CakeAddForm extends React.Component{
         const { cakeAdd } = this.state;
 
         if(this.cakeId === 'empty'){
-            
             return fetch(`https://aleciachaapp.firebaseio.com/cakes.json`, {
                         method: 'POST',
                         body: JSON.stringify(cakeAdd)
@@ -148,18 +141,9 @@ class CakeAddForm extends React.Component{
     findDataById = (data, id) => data.find((data) => data.id === id) || {};
 
     render(){
-      
-        const { classes } = this.props;
-        const {cooks, types, isLoading, saveCake, isError, error} = this.state;
-        const { name, 
-                price, 
-                priceForPortion, 
-                portionDescription,
-                cookId,
-                typeId,
-                description,
-                glutenFree,
-                imgURL} = this.state.cakeAdd;
+    
+        const {cooks, types, isLoading, saveCake, isError, error, cakeAdd} = this.state;
+        const { cookId, typeId, } = this.state.cakeAdd;
 
         const selectedCook = this.findDataById(cooks, cookId);
         const selectetType = this.findDataById(types,typeId);
@@ -168,7 +152,6 @@ class CakeAddForm extends React.Component{
             return <Redirect to={'/'}/>
         }
         
-
         if (isLoading) {
             return(<PageWrapper>
                 <CircularProgress/>
@@ -186,140 +169,21 @@ class CakeAddForm extends React.Component{
 
         if(!isLoading){
             return(<PageWrapper>
-                <Container maxWidth = "lg" >
-                
-                    <Grid >
-
-                        <Paper className = {classes.fCardHeader} >
-                            <CakeAddInput
-                                onHandleCakeChange = {this.handleCakeChange}
-                                value = {name}
-                                name = "name"
-                                label = "Nazwa ciasta: "
-                                styleProp = "header"
-                            /> 
-                        </Paper>
-                        
-                        <Grid container wrap='wrap'>
-                            
-                            <Grid item xs={12} sm={6} className = {classes.fCardWrapMedia}>
-                                {imgURL !=='' && 
-                                    <CardMedia 
-                                        image={imgURL} 
-                                        className = {classes.fCardMedia}
-                                        style={{height:'90%'}}
-                                /> }
-                                
-                                <input
-                                    accept="image/*"
-                                    style={{display:'none'}}
-                                    id="outlined-button-file"
-                                    type="file"
-                                    onChange = {this.handleFileAdd}
-                                />
-                                <label htmlFor="outlined-button-file" >
-                                    <Button variant="outlined" component="span" style={{margin: '10px'}} >
-                                        dodaj zdjęcie
-                                    </Button>
-                                </label>
-                            </Grid>
-                            
-                            <Grid container item xs={12} sm={6} direction='column' wrap="wrap">
-                                <Paper className={classes.fCardPaper}>
-                                    <CakeAddInput
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        value = {price}
-                                        name = "price"
-                                        label = "Cena zł/kg: "
-                                    />
-                                    <CakeAddInput
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        value = {priceForPortion}
-                                        name = "priceForPortion"
-                                        label = "cena za porcję/sztukę: "
-                                    />
-                                    <CakeAddInput
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        value = {portionDescription}
-                                        name = "portionDescription"
-                                        label = "porcja: "
-                                        styleDirect = {{width: "95%"}}
-                                    />  
-                                    
-                                </Paper>
-                                <Paper className={classes.fCardPaper}>
-                                    <CakeAddTypesSelect
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        types = {types}
-                                        value = {typeId}
-                                        name = "typeId"
-                                        label = "Typ ciasta: "
-                                    />
-                                    <CakeAddInput
-                                        value = {selectetType.description||""}
-                                        label = "Opis: "
-                                        rows="2"
-                                        multiline = {true}
-                                        disabled = {true}
-                                        styleDirect = {{width: "95%"}}
-                                    />
-                                    <CakeAddSelect
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        value = {glutenFree}
-                                        name = "glutenFree"
-                                        label = "bezglutenowe"
-                                        options={YESNOSELECT}
-                                    />
-                                                                    
-                                </Paper>
-                                <Paper className={classes.fCardPaper}>
-                                    <CakeAddInput
-                                        onHandleCakeChange = {this.handleCakeChange}
-                                        value = {description}
-                                        name = "description"
-                                        label = "opis ciasta:"
-                                        rows="4"
-                                        multiline = {true}
-                                        styleDirect = {{width: "95%"}}
-                                    />
-                                </Paper> 
-
-                            </Grid>
-                                
-                        </Grid>
-                        
-                        <Paper className={classes.fCardPaper}>
-                            <Grid>
-                                <CookLabelFull 
-                                    cook = {selectedCook}
-                                />
-                            </Grid>
-                        </Paper>
-                        
-                        <Button  
-                            variant="outlined" 
-                            color="secondary"
-                            style = {{margin: '20px auto'}}
-                            onClick = {this.props.history.goBack}
-                        > 
-                            powrót 
-                        </Button>
-                        <Button onClick={this.addCakeFetch}
-                                variant="outlined" 
-                                color="primary"
-                                style = {{margin: '20px'}}
-                        > 
-                            zapisz
-                        </Button>
-                        
-                    </Grid >
-                </Container>
+                <RenderCakeAddForm
+                    selectedCook = {selectedCook}
+                    selectetType = {selectetType}
+                    types = {types}
+                    cakeAdd = {cakeAdd}
+                    onHandleCakeChange = {this.handleCakeChange}
+                    handleFileAdd = {this.handleFileAdd}
+                    addCakeFetch= {this.addCakeFetch}
+                    {...this.props}
+                />
                 
             </PageWrapper>)
         }
 
-        
     }
 }
 
-export default withStyles(styles)(CakeAddForm);
+export default CakeAddForm;
---
+++ src/cakes/CakeAddForm/RenderCakeAddform.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { styles } from './CakeAddStyles';
+import { Button, CardMedia, withStyles, Paper, Grid, Container, } from '@material-ui/core';
+import CakeAddInput from './component/CakeAddInput';
+import CakeAddTypesSelect from './component/CakeAddTypesSelect';
+import CakeAddSelect from './component/CakeAddSelect';
+import { YESNOSELECT } from '../../constans/selectConstans'
+import CookLabelFull from '../CookLabelFull';
+
+function RenderCakeAddForm(props) {
+
+    const { types, selectedCook, selectetType, classes } = props;
+    const { name,
+            price,
+            priceForPortion,
+            portionDescription,
+            typeId,
+            description,
+            glutenFree,
+            imgURL } = props.cakeAdd;
+
+    return (<Container maxWidth="lg" >
+            <Grid >
+
+                <Paper className={classes.fCardHeader} >
+                    <CakeAddInput
+                        onHandleCakeChange={props.onHandleCakeChange}
+                        value={name}
+                        name="name"
+                        label="Nazwa ciasta: "
+                        styleProp="header"
+                    />
+                </Paper>
+
+                <Grid container wrap='wrap'>
+
+                    <Grid item xs={12} sm={6} className={classes.fCardWrapMedia}>
+                        {imgURL !== '' &&
+                            <CardMedia
+                                image={imgURL}
+                                className={classes.fCardMedia}
+                                style={{ height: '90%' }}
+                            />}
+
+                        <input
+                            accept="image/*"
+                            style={{ display: 'none' }}
+                            id="outlined-button-file"
+                            type="file"
+                            onChange={props.handleFileAdd}
+                        />
+                        <label htmlFor="outlined-button-file" >
+                            <Button variant="outlined" component="span" style={{ margin: '10px' }} >
+                                dodaj zdjęcie
+                                    </Button>
+                        </label>
+                    </Grid>
+
+                    <Grid container item xs={12} sm={6} direction='column' wrap="wrap">
+                        <Paper className={classes.fCardPaper}>
+                            <CakeAddInput
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                value={price}
+                                name="price"
+                                label="Cena zł/kg: "
+                            />
+                            <CakeAddInput
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                value={priceForPortion}
+                                name="priceForPortion"
+                                label="cena za porcję/sztukę: "
+                            />
+                            <CakeAddInput
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                value={portionDescription}
+                                name="portionDescription"
+                                label="porcja: "
+                                styleDirect={{ width: "95%" }}
+                            />
+
+                        </Paper>
+                        <Paper className={classes.fCardPaper}>
+                            <CakeAddTypesSelect
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                types={types}
+                                value={typeId}
+                                name="typeId"
+                                label="Typ ciasta: "
+                            />
+                            <CakeAddInput
+                                value={selectetType.description || ""}
+                                label="Opis: "
+                                rows="2"
+                                multiline={true}
+                                disabled={true}
+                                styleDirect={{ width: "95%" }}
+                            />
+                            <CakeAddSelect
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                value={glutenFree}
+                                name="glutenFree"
+                                label="bezglutenowe"
+                                options={YESNOSELECT}
+                            />
+
+                        </Paper>
+                        <Paper className={classes.fCardPaper}>
+                            <CakeAddInput
+                                onHandleCakeChange={props.onHandleCakeChange}
+                                value={description}
+                                name="description"
+                                label="opis ciasta:"
+                                rows="4"
+                                multiline={true}
+                                styleDirect={{ width: "95%" }}
+                            />
+                        </Paper>
+
+                    </Grid>
+
+                </Grid>
+
+                <Paper className={classes.fCardPaper}>
+                    <Grid>
+                        <CookLabelFull
+                            cook={selectedCook}
+                        />
+                    </Grid>
+                </Paper>
+
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    style={{ margin: '20px auto' }}
+                    onClick={props.history.goBack}
+                >
+                    powrót
+                        </Button>
+                <Button onClick={props.addCakeFetch}
+                    variant="outlined"
+                    color="primary"
+                    style={{ margin: '20px' }}
+                >
+                    zapisz
+                        </Button>
+
+            </Grid >
+        </Container>
+    )
+}
+
+export default withStyles(styles)(RenderCakeAddForm);
