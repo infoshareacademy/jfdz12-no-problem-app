4b22809d7f21f1072410809540903bd002b21f91
MalgorzataWisniewska <mwisniewska@interia.eu>
2020-01-26 19:55:06 +0100

    api2 in cooksList test

--- src/cooks/CooksList.js
+++ src/cooks/CooksList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import PageWrapper from '../components/PageWrapper';
+import { getCooks } from '../api/Api2';
 
 export default class CooksList extends Component {
+
+    state = {
+        cooks: []
+    }
+
+    componentDidMount() {
+        getCooks()
+        .then(data=>
+        this.setState({
+            cooks: data
+        })
+        )
+    }
+
     render() {
         return (
             <PageWrapper>
                 <h1>Pieczemy za Ciebie!</h1>
+                <ul>
+                {this.state.cooks.map(cook=><li>{cook.name}</li>)}
+                </ul>
             </PageWrapper>
         )
     }
