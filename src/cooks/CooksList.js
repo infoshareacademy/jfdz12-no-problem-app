b987dccd977c959c80f6fbec6412170de617f452
MalgorzataWisniewska <mwisniewska@interia.eu>
2020-02-09 15:12:55 +0100

    cooks list started

--- src/cooks/CooksList.js
+++ src/cooks/CooksList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import PageWrapper from '../components/PageWrapper';
-import { getCooks } from '../api/Api2';
+import { getCooks, getCakes } from '../api/Api2';
+import { Container, Grid } from '@material-ui/core';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default class CooksList extends Component {
 
     state = {
-        cooks: []
+        cooks: [],
+        cakes: []
     }
 
     componentDidMount() {
@@ -14,16 +21,47 @@ export default class CooksList extends Component {
         this.setState({
             cooks: data
         })
-        )
+        );
+        getCakes()
+        .then(data=>
+            this.setState({
+                cakes: data
+            }))
     }
 
+    cookCakes = [];
+
     render() {
+      
         return (
             <PageWrapper>
-                <h1>Pieczemy za Ciebie!</h1>
-                <ul>
-                {this.state.cooks.map(cook=><li>{cook.name}</li>)}
-                </ul>
+                <Container>
+                    <Grid>
+                        <h1>Pieczemy za Ciebie!</h1>
+                        
+                        {this.state.cooks.map(cook=>{
+                            let cooksCakes = this.state.cakes.filter(cake=> cake.cookID===cook.id);
+                            console.log(cooksCakes);
+                            return (
+                         <ExpansionPanel>
+                         <ExpansionPanelSummary
+                           expandIcon={<ExpandMoreIcon />}
+                           aria-controls="panel1a-content"
+                           id="panel1a-header"
+                         >
+                           <Typography>{cook.name} {cook.surname}</Typography>
+                         </ExpansionPanelSummary>
+                         <ExpansionPanelDetails>
+                           <Typography>
+                             {cook.description}
+                             {/* {cooksCakes} */}
+                           
+                           </Typography>
+                         </ExpansionPanelDetails>
+                        </ExpansionPanel>)})}
+                        
+                    </Grid>
+                </Container>
             </PageWrapper>
         )
     }
