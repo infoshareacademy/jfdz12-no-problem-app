f695472f6293c00666453802246f59e93c4ff9a2
MalgorzataWisniewska <mwisniewska@interia.eu>
2020-02-23 11:11:32 +0100

    full cooksList

--- src/cooks/CookPanel.js
+++ src/cooks/CookPanel.js
@@ -18,38 +18,26 @@ export default function CookPanel(props) {
         id={props.cook.id}
         >
             <Grid container spacing={3} alignItems='center' justify='center'>
-                <Grid item md={1} sm={4} xs={4}>
+                <Grid item md={2} sm={2} xs={2}>
                     <Avatar alt={props.cook.name} src={props.cook.avatar}  />
                 </Grid>
-                <Grid item md={4} sm={8} xs={8}>
+                <Grid item md={5} sm={5} xs={5}>
                     <Typography style={{fontWeight: 'bold', fontSize: '1.3rem'}}>{props.cook.name} {props.cook.surname}</Typography>
                 </Grid>
-                <Grid item md={2} sm={4} xs={5}>
-                    <Grid container alignItems='center'>
-                        <CallIcon/>
-                        <Typography>{props.cook.contact.mobile}</Typography>
-                    </Grid>
-                </Grid>
-                <Grid item md={3} sm={4} xs={7}>
-                    <Grid container alignItems='center' style={{minWidth:'250px'}}>
-                        <MailOutlineIcon/>
-                        <a href={`mailto:${props.cook.contact.mail}`} rel="noopener noreferrer" target="_blank" style={{textDecoration: 'none', color: 'inherit'}}>{props.cook.contact.mail}</a>
-                    </Grid>
-                </Grid>
-                <Grid item md={2} sm={4} xs={12}>
+                <Grid item md={5} sm={5} xs={5}>
                     <Typography>{props.cook.location.city} {props.cook.location.district}</Typography>
                 </Grid>
             </Grid>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
             <Grid container>
-               
                 <Grid item xs={12}>
                     <h3>O mnie</h3>
                     {props.cook.description
                     ? <p>{props.cook.description}</p>
                     : <p>Więcej o mnie już wkrótce!</p>}
                 </Grid>
+              
                 <Grid item xs={12}>
                     <h3>Moje ciasta</h3>
                 </Grid>
@@ -61,7 +49,27 @@ export default function CookPanel(props) {
                         </Grid>)
                     : <p>Już wkrótce pojawią się ciasta w ofercie!</p>}
                 </Grid>
-               
+                <Grid item xs={12}>
+                    <h3>Kontakt</h3>
+                    <Grid container direction='column' alignItems='center'>
+                      
+                            <Grid item xs={6}>
+                                <Grid container justify='center'>
+                                    <CallIcon/>
+                                    <Typography>{props.cook.contact.mobile}</Typography>
+                                </Grid>
+                                </Grid>
+                                <Grid item xs={6}>
+                                <Grid container justify='center'>
+                                    <MailOutlineIcon/>
+                                    <a href={`mailto:${props.cook.contact.mail}`} rel="noopener noreferrer" target="_blank" style={{textDecoration: 'none', color: 'inherit'}}>{props.cook.contact.mail}</a>
+                            </Grid>
+                        </Grid>
+                    
+                </Grid>
+            
+              
+                </Grid>
             </Grid>
         </ExpansionPanelDetails>
     </ExpansionPanel>
